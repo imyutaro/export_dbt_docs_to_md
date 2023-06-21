548d9f99cbba65d935c65842855d3d8a09e3bda7
imyutaro <yutarofortech@gmail.com>
2023-06-22 02:52:46 +0900

    store basic data from manifest.json and catalog.json to dataclass

--- export_dbt_docs_to_md.py
+++ export_dbt_docs_to_md.py
@@ -16,10 +16,11 @@ from json import load
 
 @dataclass(frozen=True)
 class Column:
+    type_: str
+    index: str
     name: str
-    # type_: str = None
-    description: str
-    tags: list[str]
+    comment: str
+    description_from_manifest: str = None
 
 
 @dataclass(frozen=True)
@@ -31,41 +32,65 @@ class ManifestData:
     compiled_code: str
     description: str
     language: str
+    tags: list[str]
+    package_name: str
+    database: str
+    schema: str
+    materialized: str
     # TODO: store test info for each columns
 
+def format_parsed_data_to_markdown(manifest_data: ManifestData)->str:
+    return
 
-def main():
+def parse_docs_data():
     with open("../dbt_tutorial_w_duckdb/tutorial/target/manifest.json") as f2:
         manifest_json = load(f2)
     with open("../dbt_tutorial_w_duckdb/tutorial/target/catalog.json") as f1:
         catalog_json = load(f1)
 
-    for k, v in manifest_json["nodes"].items():
+    for k, v in catalog_json["nodes"].items():
         # if k.startswith("seed.") or k.startswith("model."):
         if k.startswith("model."):
-            # print("")
-            # print(k, sort_dicts=False)
-            # print(v, sort_dicts=False)
+
+            manifest_data = manifest_json["nodes"][k]
+
             columns = []
             for i in v["columns"].values():
+                column_info_from_manifest = manifest_data["columns"].get(i["name"])
+                if column_info_from_manifest is not None:
+                    description_from_manifest = column_info_from_manifest["description"]
+                else:
+                    description_from_manifest = None
+
                 columns.append(
                     Column(
+                        type_=i["type"],
+                        index=i["index"],
                         name=i["name"],
-                        description=i["description"],
-                        tags=i["tags"],
+                        comment=i["comment"],
+                        description_from_manifest=description_from_manifest,
                     )
                 )
+
             d = ManifestData(
                 name=k,
                 columns=columns,
-                raw_code=v["raw_code"],
-                compiled_code=v["compiled_code"],
-                depends_on=v["depends_on"]["nodes"],
-                description=v["description"],
-                language=v["language"]
+                raw_code=manifest_data["raw_code"],
+                compiled_code=manifest_data["compiled_code"],
+                depends_on=manifest_data["depends_on"]["nodes"],
+                description=manifest_data["description"],
+                language=manifest_data["language"],
+                tags=manifest_data["tags"],
+                package_name=manifest_data["package_name"],
+                database=manifest_data["database"],
+                schema=manifest_data["schema"],
+                materialized=manifest_data["config"]["materialized"],
             )
-            # print(asdict(d), sort_dicts=False)
+            print(asdict(d), sort_dicts=False)
 
+    """
     for k, v in catalog_json["nodes"].items():
+        v["columns"]
+    """
 
-main()
+parse_docs_data()
